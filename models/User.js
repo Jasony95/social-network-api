e861c071ebc37435d780b8f9fcc36cff82f75a62
Jason Yang <jasony24@outlook.com>
2023-11-28 04:24:28 -0600

    first commit

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const { isEmail } = require('validator');
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: isEmail, message: 'Wrong email.'
+      },
+    },
+    thoughts: [thoughtSchema],
+    friends: [friendSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
